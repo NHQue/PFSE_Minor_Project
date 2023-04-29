2954e5b52a26c1ca160cf46d5b2859feeace058d
Niklas <haschke.niklas@web.de>
2023-04-29 15:46:32 +0200

    update

--- app.py
+++ app.py
@@ -138,8 +138,18 @@ if st.button('Calculate'):
     composite_geometry = ((box - mirrored_angle) + mirrored_angle)
     composite_geometry = composite_geometry.align_center()
     # st.pyplot(composite_geometry.plot_geometry().get_figure())
-    analysis_result = spm.perform_analysis(composite_geometry, Mxx*1000000, Vy*1000, 0)
-    st.pyplot(analysis_result.plot_stress_vm().get_figure())
+    # analysis_result = spm.perform_analysis(composite_geometry, Mxx*1000000, Vy*1000, 0)
+    # st.pyplot(analysis_result.plot_stress_vm().get_figure())
+
+
+    composite_geometry.create_mesh([20, 10])
+    sec = Section(composite_geometry, time_info=True)
+    sec.plot_mesh()
+    sec.calculate_geometric_properties()
+    sec.calculate_plastic_properties()
+    sec.calculate_warping_properties()
+    result = sec.calculate_stress(Vy=Vy*1000, Mxx=Mxx*1000000, Mzz=0)
+    st.pyplot(result.plot_stress_vm().get_figure())
 
 
 
